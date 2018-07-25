d57c84bccf80e5d58f6dd4399b6574ff6b42d7a1
q877804730 <32757968+q877804730@users.noreply.github.com>
2018-07-25 15:43:07 +0800

    Update configs.php

--- configs/configs.php
+++ configs/configs.php
@@ -6,8 +6,8 @@
  * Time: 16:19
  */
 
-define("DB_HOST","119.23.33.178");
+define("DB_HOST","localhost");
 define("DB_USER","root");
-define("DB_PWD","q877804730");
+define("DB_PWD","");
 define("DB_DBNAME","shopImooc");
 define("DB_CHARSET","utf-8");
